c344c33a257dedb0f9ae88cce0d6ae8fa4f090c6
ntt78712 <conghoang000152@gmail.com>
2023-10-03 18:48:27 +0700

    thuchanh project

---
+++ thuchanh1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace thuchanh1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
